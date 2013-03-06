2e1b6495996df1ff2478f65f31f9de1030809498
Justin Jaffray <justin.jaffray@gmail.com>
2013-03-05 23:35:23 -0500

    sequencer stuff

--- lib/sequencer.rb
+++ lib/sequencer.rb
@@ -9,13 +9,23 @@ module Ramsdel
       suffixes = puzzle_definition[:suffixes]
       axes.map { |axis| axis.map { |move| [move].product(suffixes) } }
     end
+    private :make_move_list
     
-    def opposites?(*moves)
+    def same_axis?(*moves)
       @axes.any? do |axis|
         moves.all? do |move|
           axis.map { |face| face.map(&:join) }.inject(:+).include? move
         end
       end
+    end
+
+    def same_face?(*moves)
+      moves.all? { |move| prefix(move) == prefix(moves.first) }
+    end
+
+    def prefix(move)
+      #this is probably ugly but I'm tired and I can't tell
+      move.each_char.take([1,move.length-1].max).join
     end
   end 
 end
--- spec/sequencer_spec.rb
+++ spec/sequencer_spec.rb
@@ -3,14 +3,26 @@ require_relative "spec_helper"
 describe Ramsdel::Sequencer do
   let(:sequencer) { Ramsdel::Sequencer.new(Ramsdel::Puzzles::TWO_BY_TWO) }
 
-  describe "#opposites?" do
-    it "is true if two moves are opposites" do
-      sequencer.opposites?("F","B").should eql true 
-      sequencer.opposites?("F","B'").should eql true 
+  describe "#same_axis?" do
+    it "is true if two moves are on the same axis" do
+      sequencer.same_axis?("F","B").should eql true 
+      sequencer.same_axis?("F","F").should eql true 
+      sequencer.same_axis?("F","B'").should eql true 
     end
 
-    it "is false if two moves are not opposites" do
-      sequencer.opposites?("F","R").should eql false
+    it "is false if two moves are not on the same axis" do
+      sequencer.same_axis?("F","R").should eql false
+    end
+  end
+
+  describe "#same_face?" do
+    it "is true if two moves are on the same face" do
+      sequencer.same_face?("F", "F").should eql true
+    end
+
+    it "is false if two moves are not on the same face" do
+      sequencer.same_face?("F","R").should eql false
+      sequencer.same_face?("F","B").should eql false
     end
   end
 end
