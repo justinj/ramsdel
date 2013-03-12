e2866ed25d899202ca07b516b3811e20d3b65942
Justin Jaffray <justin.jaffray@gmail.com>
2013-03-12 18:41:11 -0400

    reformatted puzzle def

--- lib/puzzles.rb
+++ lib/puzzles.rb
@@ -2,7 +2,11 @@ module Ramsdel
   module Puzzles
     THREE_BY_THREE = {
       :default_allowed => ["F","B","R","L","U","D"],
-      :axes => [["F","B","Fw","Bw"],["R","L","Rw","Lw"],["U","D","Uw","Dw"]],
+      :axes => [
+        ["F","B","Fw","Bw"],
+        ["R","L","Rw","Lw"],
+        ["U","D","Uw","Dw"]
+      ],
       :suffixes => ["","'","2"]
     }
   end
